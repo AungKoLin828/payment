0cbb35f8c1945c95550ac8a9d39657491a1d2106
AungKoLin828 <aungko.linn404@gmail.com>
2026-03-31 14:28:27 +0630

    update repo

--- src/main/java/com/payment/repository/PaymentRepository.java
+++ src/main/java/com/payment/repository/PaymentRepository.java
@@ -4,9 +4,10 @@ package com.payment.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.payment.domain.Payment;
-
+@Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
      Optional<Payment> findByReference(String reference);
 }
